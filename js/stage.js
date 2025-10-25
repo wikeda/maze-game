89df123801e843a99e6c3211f55d071ab977d826
池田 哲也 <500886@gulliver.local>
2025-10-26 00:44:49 +0900

    feat: add stage-specific colors and restore stage.js

--- js/stage.js
+++ js/stage.js
@@ -6,35 +6,35 @@ export class StageManager {
       1: { 
         size: 3, 
         name: 'Stage 1',
-        skyColor: 0x252530,
+        skyColor: 0x87ceeb,
         wallColor: '#FFFFFF',
         floorColor: '#FFFFFF'
       },
       2: { 
         size: 4, 
         name: 'Stage 2',
-        skyColor: 0x2a2530,
+        skyColor: 0x4ade80,
         wallColor: '#5a4a4a',
         floorColor: '#3f2f35'
       },
       3: { 
         size: 5, 
         name: 'Stage 3',
-        skyColor: 0x302520,
+        skyColor: 0xff6b6b,
         wallColor: '#6a3a3a',
         floorColor: '#4f1f25'
       },
       4: { 
         size: 6, 
         name: 'Stage 4',
-        skyColor: 0x252030,
+        skyColor: 0xa78bfa,
         wallColor: '#4a3a5a',
         floorColor: '#2f1f3f'
       },
       5: { 
         size: 8, 
         name: 'Stage 5',
-        skyColor: 0x1a1a15,
+        skyColor: 0xfb923c,
         wallColor: '#3a2a2a',
         floorColor: '#1f0f15'
       },
